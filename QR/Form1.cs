b95937bc3077984ab259cb30842cd84c50013415
gizemyildiz22 <ugurlucelikgizem@gmail.com>
2023-12-12 08:19:08 +0300

    Add project files.

---
+++ QR/Form1.cs
@@ -0,0 +1,32 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = "http://deneme.com";
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(str, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(5, "#FF0000", "#FFFFFF");
+            pictureBox1.Image = qrCodeImage;
+        }
+    }
+}
